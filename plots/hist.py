11e3f3aad13abf5fb548c981ce502e168d8010fc
kuntzer <thibault.kuntzer@epfl.ch>
2016-10-28 12:39:29 +0200

    tweaking of hist

--- plots/hist.py
+++ plots/hist.py
@@ -3,7 +3,6 @@ from __future__ import division
 import numpy as np
 import pylab as plt
 from scipy import stats
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def hist(ax, stars_characteristics, predictions):
 	"""
@@ -20,14 +19,14 @@ def hist(ax, stars_characteristics, predictions):
 	obs_stars = stars_characteristics[observed, 1].flatten() / .12 # Convert from Euclid small px to mas
 	ax.hist(obs_stars, binst, histtype='stepfilled', color="darkgrey", alpha=0.8)
 	
-	binst = stats.mstats.mquantiles(all_stars, [0.,0.05,0.25,0.5,0.75,0.98,1.])
+	binst = stats.mstats.mquantiles(all_stars, [0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.])
 	#binst = 6
 	nt, binst = np.histogram(all_stars, binst)
 	nd, _ = np.histogram(obs_stars, bins=binst)
 	xbin = [(binst[i] + binst[i+1])/2. for i in range(len(binst) - 1)]
 	ybin = nd/nt
-	xbin = np.concatenate([[all_stars.min()], xbin])
-	ybin = np.concatenate([[ybin[0]], ybin])
+	#xbin = np.concatenate([[all_stars.min()], xbin])
+	#ybin = np.concatenate([[ybin[0]], ybin])
 	for x,y in zip(xbin, ybin):
 		print x,y
 	
@@ -36,11 +35,9 @@ def hist(ax, stars_characteristics, predictions):
 	ax2.plot(xbin, ybin, c='k', lw=2)
 	ax2.set_ylim([0, 1])
 	
-	#axcomp.grid(True)
-	#plt.setp(axcomp.get_xticklabels(), visible=False)
 	plt.setp(ax.get_yticklabels(), visible=False)
 		
-	ax.grid(True)
+	ax2.grid(True)
 	ax.set_xlabel(r"$\mathrm{Angular\ separation\ [mas]}$")
 	ax.set_ylabel(r"$\mathrm{Occurence\ of\ binaries\ (arbit.\ units)}$")
 	
